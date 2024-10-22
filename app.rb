6a353afb58237cd816171294d7583bab752918e1
jeyden94 <jeyden94@gmail.com>
2024-10-22 00:57:36 +0000

    Initial RPS logic setup

--- app.rb
+++ app.rb
@@ -7,3 +7,87 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  comp_move = moves.sample
+
+  if comp_move == "rock"
+    outcome = "tied"
+  elsif comp_move == "paper"
+    outcome = "lost"
+  else
+    outcome = "won"
+  end
+
+  "
+  <h2>
+    We played rock!
+  </h2>
+
+  <h2>
+    They played #{comp_move}!
+  </h2>
+
+  <h2>
+    We #{outcome}!
+  </h2>
+  "
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  comp_move = moves.sample
+
+  if comp_move == "paper"
+    outcome = "tied"
+  elsif comp_move == "scissors"
+    outcome = "lost"
+  else
+    outcome = "won"
+  end
+
+  "
+  <h2>
+    We played paper!
+  </h2>
+
+  <h2>
+    They played #{comp_move}!
+  </h2>
+
+  <h2>
+    We #{outcome}!
+  </h2>
+  "
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+
+  comp_move = moves.sample
+
+  if comp_move == "scissors"
+    outcome = "tied"
+  elsif comp_move == "rock"
+    outcome = "lost"
+  else
+    outcome = "won"
+  end
+
+  "
+  <h2>
+    We played scissors!
+  </h2>
+
+  <h2>
+    They played #{comp_move}!
+  </h2>
+
+  <h2>
+    We #{outcome}!
+  </h2>
+  "
+end
