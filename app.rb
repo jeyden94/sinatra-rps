5e9a7d07c8959a326df4531210d3e6923ab04daa
jeyden94 <jeyden94@gmail.com>
2024-10-22 03:44:56 +0000

    Ready for rake grade

--- app.rb
+++ app.rb
@@ -2,12 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to Rock-Paper-Scissors!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-
-  
+  erb(:homepage)
 end
 
 get("/rock") do
